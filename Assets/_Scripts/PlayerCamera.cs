a6d8ea45169ca659662ce2b7b2c954d7a79f2f5e
starnuik <starnuik@gmail.com>
2024-02-28 18:23:20 +0300

    player's camera, movement

---
+++ Assets/_Scripts/PlayerCamera.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+using Editor = UnityEngine.SerializeField;
+
+public class PlayerCamera : MonoBehaviour
+{
+	[Editor] Transform rotationTargetY;
+	[Editor] Transform rotationTargetX;
+	[Editor] float mouseSensitivity = 1f;
+
+	private Vector2 lastMouse;
+	private float targetX;
+	private float targetY;
+
+	private void Start()
+	{
+		Cursor.lockState = CursorLockMode.Confined;
+		Cursor.visible = false;
+	}
+
+	private void Update()
+	{
+		if (Time.frameCount < 5)
+		{
+			lastMouse = Input.mousePosition;
+			return;
+		}
+		
+		var now = Input.mousePosition.ToXY();
+		var delta = now - lastMouse;
+
+		// camera Y
+		targetY = Mathf.Repeat(targetY + delta.x * mouseSensitivity, 360f);
+		{
+			var euler = rotationTargetY.localEulerAngles;
+			euler.y = targetY;
+			rotationTargetY.localEulerAngles = euler;
+		}
+
+		// camera X
+		targetX = Mathf.Clamp(targetX + -delta.y * mouseSensitivity, -89.9f, 89.9f);
+		{
+			var euler = rotationTargetX.localEulerAngles;
+			euler.x = targetX;
+			rotationTargetX.localEulerAngles = euler;
+		}
+
+		// 
+		lastMouse = now;
+	}
+}
---
+++ Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Editor = UnityEngine.SerializeField;
+
+public class PlayerMovement : MonoBehaviour
+{
+	[Editor] Transform player;
+	[Editor] NavMeshAgent navAgent;
+	[Editor] float moveSpeed = 1f;
+
+	private void Update()
+	{
+		var move = Vector3.zero;
+		move.z += Input.GetKey(KeyCode.W) ? 1f : 0f;
+		move.z += Input.GetKey(KeyCode.S) ? -1f : 0f;
+		move.x += Input.GetKey(KeyCode.D) ? 1f : 0f;
+		move.x += Input.GetKey(KeyCode.A) ? -1f : 0f;
+		
+		var worldMove = player.TransformDirection(move);
+		var delta = worldMove * moveSpeed * Time.deltaTime;
+		navAgent.Move(delta);
+	}
+}
