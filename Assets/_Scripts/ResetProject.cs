a5ed972cac656be227be77d229df216d8ff3eeec
StarNuik <starnuik@gmail.com>
2024-03-21 18:47:40 +0300

    small tweaks

--- Assets/_Scripts/ResetProject.cs
+++ Assets/_Scripts/ResetProject.cs
@@ -10,14 +10,11 @@ public class ResetProject : MonoBehaviour
 {
 	[Editor] Image white;
 	[Editor] Image black;
-	[Editor] Material screen;
 
 	[Button]
 	private void Reset()
 	{
 		white.SetAlpha(0f);
 		black.SetAlpha(0f);
-
-		screen.SetColor("_EmissionColor", Color.black);
 	}
 }
