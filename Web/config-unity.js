e80694296fff1fe3d1058cf8d780215fddc75728
StarNuik <starnuik@gmail.com>
2024-04-03 19:30:21 +0300

    Update config-unity.js

--- Web/config-unity.js
+++ Web/config-unity.js
@@ -11,12 +11,12 @@ let config = {
 };
 
 let elemCanvas = document.querySelector("#unity-canvas");
-let elemProgressFull = document.querySelector("#unity-progress-bar-full");
-let elemCover = document.querySelector("#loading-cover");
 
 // used by Unity
 var SetLoadProgress = function(progress)
 {
+	// console.log(`[ SetLoadProgress ] ${progress}`);
+	
 	let remapped = 0.0;
 	if (progress <= 1.0)
 	{
@@ -27,13 +27,17 @@ var SetLoadProgress = function(progress)
 		remapped = 0.75 + 0.25 * (progress - 1.0);
 	}
 
+	let elemProgressFull = document.querySelector("#unity-progress-bar-full");
 	elemProgressFull.style.width = `${100 * remapped}%`;
 };
 
-var SetLoaded = function()
+var NotifyLoaded = function()
 {
+	// console.log("[ NotifyLoaded ]");
+
+	let elemCover = document.querySelector("#loading-cover");
 	elemCover.style.display = "none";
-}
+};
 
 let script = document.createElement("script");
 script.src = loaderUrl;
