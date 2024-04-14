b60417396c5b8f6338d1c50ffb7b6ec07752ff72
StarNuik <starnuik@gmail.com>
2024-04-14 19:23:23 +0300

    render scale fix

--- Assets/_Scripts/Services/RenderScaleService.cs
+++ Assets/_Scripts/Services/RenderScaleService.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 using Editor = UnityEngine.SerializeField;
 
 public class RenderScaleService : MonoBehaviour
 {
+	[Editor] UniversalRenderPipelineAsset pipeline;
 	[Editor] FloatPlatformDependent renderScale;
 
 	private void Start()
 	{
 		var scale = renderScale.Value;
-		ScalableBufferManager.ResizeBuffers(scale, scale);
+		pipeline.renderScale = scale;
 	}
 }
